cf6dcbade25e015b8279af3b1e0887f6af270b44
Eric Van Albert <eric@van.al>
2020-03-06 20:53:31 -0500

    Start adding SPI HAL

--- driver-firmware/src/main.c
+++ driver-firmware/src/main.c
@@ -6,22 +6,10 @@ int main(void) {
     protocol_init();
     hal_init();
     output_init();
-    //for (;;) {
-    //    output_length[0] = 1024 * 3;
-    //    output_length[1] = 1024 * 3;
-    //    output_length[2] = 1024 * 3;
-    //    output_length[3] = 1024 * 3;
-    //    output_length[4] = 1024 * 3;
-    //    output_length[5] = 1024 * 3;
-    //    output_length[6] = 1024 * 3;
-    //    output_length[7] = 1024 * 3;
-    //    output_write();
-    //}
+
+    // SD code here
+
     for (;;) {
-        //for (int i = 0; i < 300; i++) {
-        //    hal_poll_usb(); // Give USB an advantage
-        //}
-        //otg_fs_isr();
         output_update_indicators();
     }
 }
